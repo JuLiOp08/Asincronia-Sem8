81da03700129a55af6ea4772bef8501ea148ca93
naye-gg <nayeli.guerrero@utec.edu.pe>
2025-05-13 17:26:13 -0500

    implementando emailNotificationListener

---
+++ src/main/java/com/example/eventosconwhereby/domain/Listener/EmailNotificationListener.java
@@ -0,0 +1,17 @@
+package com.example.eventosconwhereby.domain.Listener;
+
+import org.springframework.context.event.EventListener;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class EmailNotificationListener {
+    private static final Logger logger = LogManager.getLogger(EmailNotificationListener.class);
+
+    @EventListener
+    public void handleOrderCreatedEvent(OrderCreatedEvent event) {
+        logger.info("Enviando correo a: {} - Pedido ID: {} - Productos: {}", 
+            event.getEmail(), 
+            event.getOrderId(), 
+            event.getProducts());
+    }
+}
