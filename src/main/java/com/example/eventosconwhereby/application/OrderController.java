766769f61661934a6ceb87088b79ab3971d7921b
Yeimi Varela <yeimi.varela@utec.edu.pe>
2025-05-13 16:59:03 -0500

    OrderController mejorado

--- src/main/java/com/example/eventosconwhereby/application/OrderController.java
+++ src/main/java/com/example/eventosconwhereby/application/OrderController.java
@@ -1,14 +1,12 @@
 package com.example.eventosconwhereby.application;
 
+import com.example.eventosconwhereby.domain.OrderCreatedEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.data.domain.jaxb.SpringDataJaxb;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Random;
 
@@ -16,15 +14,19 @@ import java.util.Random;
 @RequestMapping("/orders")
 public class OrderController {
 
+
+    private final ApplicationEventPublisher eventPublisher;
+
     @Autowired
-    private ApplicationEventPublisher eventPublisher;
+    public OrderController(ApplicationEventPublisher eventPublisher) {
+        this.eventPublisher = eventPublisher;
+    }
 
     @PostMapping
-    public ResponseEntity<Void> createOrder(@RequestBody SpringDataJaxb.OrderDto dto) {
-        // imular guardado
-        Long generatedOrderId = new Random().nextLong();
-
-        eventPublisher.publishEvent(new OrderCreatedEvent(this, generatedOrderId, dto.getEmail(), dto.getProducts()));
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+    public String createOrder(@RequestParam String orderId, @RequestParam String email, @RequestParam String productList) {
+        // Publicando el evento OrderCreatedEvent
+        OrderCreatedEvent event = new OrderCreatedEvent(this, orderId, email, productList);
+        eventPublisher.publishEvent(event);
+        return "Evento publicado: " + event;
     }
 }
