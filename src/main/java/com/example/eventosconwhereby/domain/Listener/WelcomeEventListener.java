236c9c7318c10696e1a9b540ad7ceed94bcdc3d6
Yeimi Varela <yeimi.varela@utec.edu.pe>
2025-05-13 17:20:35 -0500

    WelcomeEventListener creado

--- src/main/java/com/example/eventosconwhereby/domain/Listener/WelcomeEventListener.java
+++ src/main/java/com/example/eventosconwhereby/domain/Listener/WelcomeEventListener.java
@@ -1,4 +1,18 @@
 package com.example.eventosconwhereby.domain.Listener;
 
+import com.example.eventosconwhereby.domain.OrderCreatedEvent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
 public class WelcomeEventListener {
+
+    private static final Logger logger = LogManager.getLogger(WelcomeEventListener.class);
+
+    @EventListener
+    public void handleOrderCreated(OrderCreatedEvent event) {
+        logger.info("📧 Enviando correo a: {}", event.getEmail());
+    }
 }
