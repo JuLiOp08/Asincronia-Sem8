6a7bf00a0a427663f06cdc3e40172529c2bfe325
Yeimi Varela <yeimi.varela@utec.edu.pe>
2025-05-13 17:36:35 -0500

    Anotacion añadida

--- src/main/java/com/example/eventosconwhereby/domain/Listener/EmailNotificationListener.java
+++ src/main/java/com/example/eventosconwhereby/domain/Listener/EmailNotificationListener.java
@@ -4,15 +4,17 @@ import com.example.eventosconwhereby.domain.OrderCreatedEvent;
 import org.springframework.context.event.EventListener;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Component;
 
+@Component
 public class EmailNotificationListener {
     private static final Logger logger = LogManager.getLogger(EmailNotificationListener.class);
 
     @EventListener
     public void handleOrderCreatedEvent(OrderCreatedEvent event) {
-        logger.info("Enviando correo a: {} - Pedido ID: {} - Productos: {}", 
-            event.getEmail(), 
-            event.getOrderId(), 
+        logger.info("Enviando correo a: {} - Pedido ID: {} - Productos: {}",
+            event.getEmail(),
+            event.getOrderId(),
             event.getProductList());
     }
 }
