250f16e9e864cdff57840815da31e3d68b7f6cfb
JuLiOp08 <julio.portugal@utec.edu.pe>
2025-05-13 17:10:58 -0500

    Creando el AudiLogListener para registrar los pedidos en logs

---
+++ src/main/java/com/example/eventosconwhereby/domain/Listener/AuditLogListener.java
@@ -0,0 +1,19 @@
+package com.example.eventosconwhereby.domain.Listener;
+
+import com.example.eventosconwhereby.domain.OrderCreatedEvent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuditLogListener {
+
+    private static final Logger logger = LogManager.getLogger(AuditLogListener.class);
+
+    @EventListener
+    public void handleOrderCreatedEvent(OrderCreatedEvent event) {
+        logger.info("[AuditLog] Pedido registrado: ID={}, Email={}, Productos={}",
+                event.getOrderId(), event.getEmail(), event.getProductList());
+    }
+}
